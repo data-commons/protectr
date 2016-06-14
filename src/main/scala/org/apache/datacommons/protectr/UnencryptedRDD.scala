ca55a5acae1591c1b86c371b5381366e7315b1ea
Lalit Pandey <lalitpan@thoughtworks.com>
2016-06-14 17:30:43 +0530

    changed the version of spark in pom.xml

--- src/main/scala/org/apache/datacommons/protectr/UnencryptedRDD.scala
+++ src/main/scala/org/apache/datacommons/protectr/UnencryptedRDD.scala
@@ -1,6 +1,15 @@
 package org.apache.datacommons.protectr
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkContext, SparkConf}
 
-class UnencryptedRDD {
 
+object UnencryptedRDD {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("SparkJoins").setMaster("local")
+    val context = new SparkContext(conf)
+
+    println("hello")
+  }
 }
