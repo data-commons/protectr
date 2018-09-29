896e110e386d4c80562edb07a2eb45609f2807aa
ashishJoy <ashishkujoy@gmail.com>
2018-09-29 17:50:49 +0530

    [Ashish Joy] Refactor HomomorphicallyEncryptedRDD by pulling private key as variable and reusing it whenever required

--- src/main/scala/org/apache/datacommons/protectr/rdds/HomomorphicallyEncryptedRDD.scala
+++ src/main/scala/org/apache/datacommons/protectr/rdds/HomomorphicallyEncryptedRDD.scala
@@ -2,46 +2,53 @@ package org.apache.datacommons.protectr.rdds
 
 import java.math.BigInteger
 
-import com.n1analytics.paillier.{PaillierPrivateKey, EncryptedNumber}
+import com.n1analytics.paillier.{EncryptedNumber, PaillierPrivateKey}
 import org.apache.datacommons.protectr.encryptors.EncryptionKeyPair
 import org.apache.datacommons.protectr.types.FileType
 import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{Partition, TaskContext}
 
-class HomomorphicallyEncryptedRDD
-(RDD: RDD[String], keyPair: EncryptionKeyPair, fileType: FileType) extends RDD[String](RDD) {
+class HomomorphicallyEncryptedRDD(
+                                   RDD: RDD[String],
+                                   keyPair: EncryptionKeyPair,
+                                   fileType: FileType) extends RDD[String](RDD) {
+
+  private val privateKey: PaillierPrivateKey = keyPair.getPrivateKey
 
   def sum(columnIndex: Int): BigInteger = {
-    val finalRecord = this.reduce((firstRow, secondRow) => {
+    val finalRecord: String = this.reduce((firstRow, secondRow) => {
+
       val firstRecord: Array[String] = fileType.parseRecord(firstRow)
       val secondRecord: Array[String] = fileType.parseRecord(secondRow)
+
       val firstNumber: EncryptedNumber = EncryptedNumber.create(
-        firstRecord(columnIndex), keyPair.getPrivateKey)
+        firstRecord(columnIndex), privateKey)
       val secondNumber: EncryptedNumber = EncryptedNumber.create(
-        secondRecord(columnIndex), keyPair.getPrivateKey)
+        secondRecord(columnIndex), privateKey)
+
       firstRecord(columnIndex) = firstNumber.add(secondNumber).toString
       fileType.join(firstRecord)
     })
+
     val sum: String = fileType.parseRecord(finalRecord)(columnIndex)
-    val result: EncryptedNumber = EncryptedNumber.create(sum, keyPair.getPrivateKey)
-    result.decrypt(keyPair.getPrivateKey).decodeApproximateBigInteger
+    val result: EncryptedNumber = EncryptedNumber.create(sum, privateKey)
+    result.decrypt(privateKey).decodeApproximateBigInteger
   }
 
   def decrypt(columnIndex: Int): UnencryptedRDD = {
-    val privateKey: PaillierPrivateKey = keyPair.getPrivateKey
-    val javaRDD = this.map(row =>{
-        val values: Array[String] = fileType.parseRecord(row)
-        val encryptedNumber: EncryptedNumber = EncryptedNumber.create(
-          values(columnIndex), keyPair.getPrivateKey)
-        val bigInteger: BigInteger = privateKey.decrypt(encryptedNumber).decodeApproximateBigInteger
-        values(columnIndex) = bigInteger.toString
-        fileType.join(values)
-      })
-    new UnencryptedRDD(javaRDD,fileType)
+    val javaRDD = this.map(row => {
+      val values: Array[String] = fileType.parseRecord(row)
+      val encryptedNumber: EncryptedNumber = EncryptedNumber.create(
+        values(columnIndex), privateKey)
+      val bigInteger: BigInteger = privateKey.decrypt(encryptedNumber).decodeApproximateBigInteger
+      values(columnIndex) = bigInteger.toString
+      fileType.join(values)
+    })
+    new UnencryptedRDD(javaRDD, fileType)
   }
 
-  override protected def getPartitions = RDD.partitions
+  override protected def getPartitions: Array[Partition] = RDD.partitions
 
   @DeveloperApi
   override def compute(split: Partition, context: TaskContext): Iterator[String] = {
